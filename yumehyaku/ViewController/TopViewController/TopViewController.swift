d4ec1fabadd74a4e09b8235d1a5f18b2093cc0b4
kazukiomori <kazuki1110170501@gmail.com>
2023-05-15 23:45:56 +0900

    ViewController、ViewModel、Modelのグループを作成

--- yumehyaku/ViewController/TopViewController/TopViewController.swift
+++ yumehyaku/ViewController/TopViewController/TopViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TopViewController.swift
 //  yumehyaku
 //
 //  Created by Kazuki Omori on 2023/05/15.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TopViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
