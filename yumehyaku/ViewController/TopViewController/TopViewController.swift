34ce70615d134ad8fd458b4614427f17b7fb6621
kazukiomori <kazuki1110170501@gmail.com>
2023-05-17 22:56:34 +0900

    tableViewの設定

--- yumehyaku/ViewController/TopViewController/TopViewController.swift
+++ yumehyaku/ViewController/TopViewController/TopViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class TopViewController: UIViewController {
-
+class TopViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var bannerView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var navigationBar: UINavigationBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
 }
 
